1c000999a904db17fb3aff29889458d817fe7d97
cosmos <dhanushdata@gmail.com>
2022-03-24 22:59:01 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello from Kapstone - React basic web
         </p>
         <a
           className="App-link"
